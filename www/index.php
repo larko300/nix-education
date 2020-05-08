2a0534cafadf2a7e0a4bb139f69a2de65b426c6b
Artem Lukianov <artem.lukianov11@gmail.com>
2020-05-08 13:50:14 +0300

    first task

---
+++ www/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="public/styles/index.css">
+    <title>Nix education</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">header</div>
+        <div class="nav">
+            <ul>
+                <li><a href="">Lesson 1</a></li>
+                <li><a href="">Lesson 2</a></li>
+                <li><a href="">Lesson 3</a></li>
+            </ul>
+        </div>
+        <div class="container">
+            <div class="main">main</div>
+        </div>
+        <div class="footer">footer</div>
+    </div>
+</body>
+</html>
